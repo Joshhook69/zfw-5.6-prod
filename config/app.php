ff42c31e5a76411c30481c9cd7003db21fbc4956
Joshhook69 <40162974+Joshhook69@users.noreply.github.com>
2019-01-08 09:05:32 -0600

    Update app.php

--- config/app.php
+++ config/app.php
@@ -129,6 +129,7 @@ return [
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
+        //Illuminate\Support\Facades\RouteServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
